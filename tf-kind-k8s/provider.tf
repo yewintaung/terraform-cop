bde358e48056e1c7e3adef5f88e2ecda2f44e732
yewintaung <yewintaung.ucsm@gmail.com>
2024-05-22 23:15:46 +0800

    masking cred

--- tf-kind-k8s/provider.tf
+++ tf-kind-k8s/provider.tf
@@ -5,7 +5,7 @@ terraform {
     }
   }
   backend "pg" {
-    conn_str = "postgres://admin:Admin123456@178.128.101.207/terraform_backend?sslmode=disable"
+    conn_str = "postgres://username:pwd@hostname/db_name?sslmode=disable"
   }
 }
 
